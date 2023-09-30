22fc1b30112db570190ffc453ecac31d2ca64097
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-30 19:30:01 +0900

    エラーメッセージの表示・非表示を確認するテストコードを実装

--- storybook-7-vue-3-sample/src/stories/SignUpForm.stories.ts
+++ storybook-7-vue-3-sample/src/stories/SignUpForm.stories.ts
@@ -1,6 +1,7 @@
 import SignUpForm from '../components/SignUpForm.vue';
 import type { Meta, StoryObj } from '@storybook/vue3';
 import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 type Story = StoryObj<typeof SignUpForm>;
 
@@ -30,6 +31,10 @@ export const Complete: Story = {
     await userEvent.clear(ageInput);
     await userEvent.type(ageInput, "30");
     await userEvent.click(submitButton);
+
+    // エラーメッセージが表示されていないことを確認
+    expect(await canvas.queryByText('名前を入力してください')).toBeNull();
+    expect(await canvas.queryByText('18歳未満は登録できません')).toBeNull();
   },
 };
 
@@ -45,6 +50,10 @@ export const Error: Story = {
     await userEvent.clear(ageInput);
     await userEvent.type(ageInput, "17");
     await userEvent.click(submitButton);
+
+        // エラーメッセージが表示されていることを確認
+        expect(await canvas.queryByText('名前を入力してください')).toBeTruthy();
+        expect(await canvas.queryByText('18歳未満は登録できません')).toBeTruthy();
   },
 };
 
