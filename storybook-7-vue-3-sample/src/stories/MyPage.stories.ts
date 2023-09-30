a0e2d569fe18c06491298d12184c42dd16cc9a15
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-30 16:22:22 +0900

    全ストーリーを跨いだ共通のレイアウトを定義

--- storybook-7-vue-3-sample/src/stories/MyPage.stories.ts
+++ storybook-7-vue-3-sample/src/stories/MyPage.stories.ts
@@ -1,55 +1,27 @@
-import MyPage from '../components/MyPage.vue';
-import type { Meta, StoryObj } from '@storybook/vue3';
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import MyPage from "../components/MyPage.vue";
+import type { Meta, StoryObj } from "@storybook/vue3";
 
 type Story = StoryObj<typeof MyPage>;
 
 const meta: Meta<typeof MyPage> = {
-  title: 'MyPage',
+  title: "MyPage",
   component: MyPage,
   render: () => ({
     components: { MyPage },
     template: "<MyPage />",
   }),
-  // PC、Mobile それぞれのビューポートを作成（高さは指定しない）
-  parameters: {
-    viewport: {
-      viewports: {
-        pc: {
-          name: "Min PC Layout",
-          styles: {
-            width: "992px",
-            height: "100%",
-          },
-        },
-        mobile: {
-          name: "Min Mobile Layout",
-          styles: {
-            width: "375px",
-            height: "100%",
-          },
-        },
-      },
-    },
-  },
 };
 
-export const ForPc: Story = {
-  parameters: {
-    viewport: {
-      defaultViewport: 'pc',
-    },
-  },
-};
+// デフォルトで PC レイアウトになるので、ここでは何も指定しない
+export const ForPc: Story = {};
 
+// デフォルト設定を上書きしてモバイルレイアウトにする
 export const ForMobile: Story = {
   parameters: {
     viewport: {
-      defaultViewport: 'mobile',
+      defaultViewport: "mobile",
     },
   },
 };
-
-// export const Default: Story = {};
 
 export default meta;
